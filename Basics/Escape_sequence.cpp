95e5257fe10de7f085a72f9e0992025bfbb3f318
sagorrkr <ridoykum9@gmail.com>
2024-05-26 10:40:58 +0600

    escape sequence

---
+++ Basics/Escape_sequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+ 
+/*
+\a	Alert (bell, alarm)
+\b	Backspace
+\f	Form feed (new page)
+\n	New-line
+\r	Carriage return
+\t	Horizontal tab
+\v	Vertical tab
+\'	Single quotation mark
+\"	Double quotation mark
+\?	Question mark
+\\	Backslash
+*/
+
+int main()
+{
+    cout << "Hello World" <<endl << "This is a nice day." <<endl;
+    cout << "This is Roy \n"<<  "This is a new repo\n"<< "It's Sunday" <<endl;
+    return 0;
+
+}
