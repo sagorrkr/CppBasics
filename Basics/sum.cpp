27d2c59556720f1974cfe7ed3729618e8483354d
sagorrkr <ridoykum9@gmail.com>
2025-02-12 21:27:37 +0800

    basic variable

---
+++ Basics/sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int num1, num2;
+
+num1 = 20;
+num2 = 30;
+
+int sum = num1 + num2;
+
+cout<< "Sum is: "<< sum<< endl;
+
+
+
+    return 0;
+}
