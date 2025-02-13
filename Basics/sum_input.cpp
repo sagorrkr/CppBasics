a9f65f807329da2a0b2a242d6ee99e0b4eff6604
sagorrkr <ridoykum9@gmail.com>
2025-02-13 20:04:40 +0800

    user input

---
+++ Basics/sum_input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+   
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    
+    cout << "Sum is: " << sum << endl;
+
+    return 0;
+}
