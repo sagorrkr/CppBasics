52f041a82dd4fa8ca74626d64b4df324e72b1a7c
sagorrkr <ridoykum9@gmail.com>
2025-02-14 12:46:25 +0800

    float & char

---
+++ Basics/float_char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1 = 10.5, num2 = 20.5088888872;
+    char ch = 'A'; 
+
+    cout << "Number1 = " << num1 << endl;
+    cout << "Number2 = " << num2 << endl;
+    cout << "Character = " << ch << endl;
+
+    return 0;
+}
